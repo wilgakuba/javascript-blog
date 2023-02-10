013cd83289b9dccf358caa64431e3b89e8078903
Jakub Wilga <wilga327@wp.pl>
2023-02-10 20:07:06 +0100

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /*  [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+    }
   
     /* add class 'active' to the clicked link */
+
+    clickedElement.classList.add('active');
+
+    console.log('clickedElement:', clickedElement);
   
     /* remove class 'active' from all articles */
   
